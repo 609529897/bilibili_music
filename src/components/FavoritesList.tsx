95c73fd4ae484942c50695a161b6af2f1b4b4363
609529897 <609529897@qq.com>
2024-12-30 22:06:39 +0800

    样式

--- src/components/FavoritesList.tsx
+++ src/components/FavoritesList.tsx
@@ -1,6 +1,5 @@
 import { useEffect } from 'react';
 import { clsx } from 'clsx';
-import RefreshIcon from './RefreshIcon';
 import LoadingSpinner from './LoadingSpinner';
 
 interface Favorite {
@@ -70,7 +69,16 @@ export const FavoritesList = ({
               disabled={isLoading}
               title="刷新收藏夹"
             >
-              <RefreshIcon className={clsx("w-4 h-4", isLoading && "animate-spin")} />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className={`w-4 h-4 ${isLoading ? "animate-spin" : ""}`}
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="M12 20q-3.35 0-5.675-2.325T4 12q0-3.35 2.325-5.675T12 4q1.725 0 3.3.712T18 6.75V4h2v7h-7V9h4.2q-.8-1.4-2.187-2.2T12 6Q9.5 6 7.75 7.75T6 12q0 2.5 1.75 4.25T12 18q1.925 0 3.475-1.1T17.65 14h2.1q-.7 2.65-2.85 4.325T12 20Z"
+                />
+              </svg>
             </button>
           </div>
           {avatarUrl ? (
