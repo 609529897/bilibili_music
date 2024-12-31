7ea8add86fae5b7c325c164ceba744eb937b5dd6
609529897 <609529897@qq.com>
2024-12-31 21:58:09 +0800

    播放键

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -211,7 +211,7 @@ export const ModernPlayer: React.FC<ModernPlayerProps> = ({
                 ) : isPlaying ? (
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6"
+                    className="w-7 h-7"
                     viewBox="0 0 24 24"
                   >
                     <path
