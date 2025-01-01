ac9c9707ca909530fd6a465d50fc76b51ffba83c
609529897 <609529897@qq.com>
2025-01-01 20:05:23 +0800

    logo

--- src/components/LoginScreen.tsx
+++ src/components/LoginScreen.tsx
@@ -8,9 +8,19 @@ export const LoginScreen = ({ isLoading, error, onLogin }: LoginScreenProps) => 
   return (
     <div className="h-screen flex items-center justify-center">
       <div className="text-center">
-        <svg xmlns="http://www.w3.org/2000/svg" className="w-24 h-24 text-pink-500 mx-auto mb-6" viewBox="0 0 24 24">
-          <path fill="currentColor" d="M12 3v10.55c-.59-.34-1.27-.55-2-.55c-2.21 0-4 1.79-4 4s1.79 4 4 4s4-1.79 4-4V7h4V3h-6Z"/>
-        </svg>
+        <div className="mb-6 flex flex-col items-center">
+          <div className="relative">
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-12 h-12 text-pink-500" viewBox="0 0 24 24">
+              <path fill="currentColor" d="M9 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3H9z"/>
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-10 h-10 text-pink-400 absolute -right-4 -top-2" viewBox="0 0 24 24">
+              <path fill="currentColor" d="M9 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3H9z"/>
+            </svg>
+          </div>
+          <span className="mt-3 text-2xl font-bold tracking-wide bg-gradient-to-r from-pink-500 to-pink-600 bg-clip-text text-transparent">
+            BiliMusic
+          </span>
+        </div>
         <p className="text-gray-500 mb-8">请先登录以继续使用</p>
         <button
           onClick={onLogin}
