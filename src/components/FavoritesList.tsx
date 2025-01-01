9722928c35a6527288c556833c58bcab209ea0da
609529897 <609529897@qq.com>
2025-01-01 13:09:46 +0800

    大小

--- src/components/FavoritesList.tsx
+++ src/components/FavoritesList.tsx
@@ -107,7 +107,7 @@ export const FavoritesList = ({
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6"
+                className="w-5 h-5"
                 viewBox="0 0 24 24"
               >
                 <path
@@ -124,7 +124,7 @@ export const FavoritesList = ({
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className={`w-6 h-6 ${isLoading ? "animate-spin" : ""}`}
+                className={`w-5 h-5 ${isLoading ? "animate-spin" : ""}`}
                 viewBox="0 0 24 24"
               >
                 <path
